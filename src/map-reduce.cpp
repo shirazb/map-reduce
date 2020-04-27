e6ef85f8968ccf9045c4f08e8169033d4ff91a30
Shiraz Butt <shiraz.b@icloud.com>
2020-04-27 22:06:34 +0100

    Add comment explaining current types of execute and map_func

--- src/map-reduce.cpp
+++ src/map-reduce.cpp
@@ -9,7 +9,18 @@ void MapReduce::emit_intermediate(int key, int value) {
     this->intermediates[key].emplace_back(value);
 }
 
-void MapReduce::map(std::initializer_list<int> data) {
+void MapReduce::execute(std::initializer_list<int> data) {
+    /*
+     * Usually, this function will take a single document that will be sent to
+     * map. Later, it will be a distributed document, which we will split and
+     * send to multiple map functions, which still take the type of a single
+     * document, though.
+     *
+     * Here, for convenience, our "document" is a list of ints, and each map
+     * function operates not on a (presumably smaller) list of ints, but
+     * on a single int.
+     */
+
     for (auto x: data) {
         this->map_func(x, x);
     }
