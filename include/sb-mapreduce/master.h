cb2b4dad2897d3feaf7a66d1f0060f5b11ad652b
Shiraz Butt <shiraz.b@icloud.com>
2020-05-02 21:47:17 +0100

    master copy move operations

--- include/sb-mapreduce/master.h
+++ include/sb-mapreduce/master.h
@@ -11,6 +11,7 @@ using UserMapFunc = void(*)(int, int);
 using IntermediateHashFunc = int (*)(int);
 
 class Master {
+public:
     Master(
             std::vector<std::string> input_files,
             std::vector<std::string> output_files,
@@ -18,6 +19,12 @@ class Master {
             int num_workers,
             IntermediateHashFunc intermediate_hash
     );
+
+    Master(const Master& m) =delete;
+    Master& operator=(const Master& m) =delete;
+
+    Master(Master&& m) =default;
+    Master& operator=(Master&& m) =default;
 
     void go();
 
