2f0281b5569e1c6e3bacbfe0afb5a20e20e6bf21
Shiraz Butt <shiraz.b@icloud.com>
2020-05-07 22:38:55 +0100

    EmitIntermediateStream and EmitIntermediateStream iterator, extracted out into the own files
    Cannot have method definitions of both classes use each other as value type in same header - had to extract into own header-source files.

--- include/sb-mapreduce/common.h
+++ include/sb-mapreduce/common.h
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <list>
 #include <vector>
-#include <utility>
 
 namespace shiraz::MapReduce {
 
@@ -18,73 +17,6 @@ using UserMapFunc = void(*)(std::ifstream&, EmitIntermediateStream&);
 using UserReduceFunc = void(*)(std::string, std::list<std::string>, EmitResultStream&);
 
 using IntermediateHashFunc = int(*)(int);
-
-
-template<typename K_i = std::string, typename V_i = std::string>
-class IntermediateResult: public std::pair<K_i, V_i> {
-public:
-    using std::pair<K_i, V_i>::pair;    
-};
-
-template<typename K_i, typename V_i>
-std::ostream&
-operator<<(std::ostream& os, const IntermediateResult<K_i, V_i>& itr) {
-    os << itr.first << "," << itr.second << std::endl;
-    return os;
-}
-
-class EmitIntermediateStream {
-public:
-    EmitIntermediateStream(
-            IntermediateHashFunc hash_inter,
-            std::vector<std::ofstream> ofss
-    ) :
-            hash_inter{hash_inter},
-            ofss{std::move(ofss)}
-    {}
-
-    /**
-     * For convenience, and to hide type of output iterator created. In
-     * particular, hide the delimiter parameter passed to constructor.
-     */
-    template<typename K_i = std::string, typename V_i = std::string>
-    std::ostream_iterator<IntermediateResult<K_i, V_i>>
-    begin() {
-        return std::ostream_iterator<IntermediateResult<K_i, V_i>>{this->ofs};
-    }
-
-    template<typename K_i, typename V_i>
-    EmitIntermediateStream&
-    operator<<(IntermediateResult<K_i, V_i> ir) {
-        const auto r = this->hash_inter(ir.first);
-        this->ofss[r] << ir.first << "," << ir.second << std::endl;
-        return *this;
-    }
-
-    template<typename K_i, typename V_i>
-    void
-    to_stream(IntermediateResult<K_i, V_i> ir) {
-        this << ir;
-    }
-
-    bool
-    operator!() const {
-        return !this->ofs;
-    }
-
-    explicit
-    operator bool() const {
-        return (bool) this->ofs;
-    }
-
-    // Is movable, not copyable. Copy already deleted in superclass.
-    EmitIntermediateStream(EmitIntermediateStream&&) =default;
-    EmitIntermediateStream& operator=(EmitIntermediateStream&&) =default;
-
-private:
-    IntermediateHashFunc hash_inter;
-    std::vector<std::ofstream> ofss;
-};
 
 namespace utils {
 
