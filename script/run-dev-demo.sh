d289e0f54117c8970ef9e1446ca47edacb63dc3d
Shiraz Butt <shiraz.b@icloud.com>
2020-05-18 22:40:34 +0100

    Update run-dev-demo script to call dev-demo instead of old dev_demo

--- script/run-dev-demo.sh
+++ script/run-dev-demo.sh
@@ -1,4 +1,4 @@
 WORKDIR=${1:-~/Programming/.cmake_builds/sb-mapreduce/debug}
 
 # https://stackoverflow.com/questions/786376/how-do-i-run-a-program-with-a-different-working-directory-from-current-from-lin
-(cd "${WORKDIR}" && exec exe/dev_demo)
+(cd "${WORKDIR}" && exec exe/dev-demo)
